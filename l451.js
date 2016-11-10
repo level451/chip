cda6920f94b98851cbacb54b6d084d8c8606d940
Todd <twitzel@gmail.com>
2016-11-10 14:01:14 -0700

    added package json

--- l451.js
+++ l451.js
@@ -35,7 +35,7 @@ function openSerialPort(portname)
     });
 
     serialPort.on('data', function(data) {
-        sbuffer += data.toString();
+        sbuffer += data
         console.log(sbuffer);
         console.log(sbuffer.indexOf('\r'))
         if (sbuffer.indexOf('\r')  > 0){
