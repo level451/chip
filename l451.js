da23603b2839f7c7c31f92dcd72d0a2fdb54a8e5
Todd <twitzel@gmail.com>
2016-11-10 13:27:39 -0700

    added package json

--- l451.js
+++ l451.js
@@ -2,6 +2,13 @@ var com = require('serialport');
 //openSerialPort('/dev/ttyS0');
 openSerialPort('/dev/ttyUSB0');
 console.log('wroking?')
+process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+if (chunk !== null) {
+    commandline(chunk);
+}
+});
+
 function openSerialPort(portname)
 {
     // console.log("Attempting to open serial port "+portname);
@@ -29,6 +36,7 @@ function openSerialPort(portname)
 
     serialPort.on('data', function(data) {
         console.log(data)
+
         console.log(data.toString())
 
     });
@@ -48,3 +56,24 @@ exports.serialToObject = function(data) {
     var sdata = data.split(" ");
     return sdata
 };
+function commandline(s){
+    s = s.toString();
+    t = s.replace(',',' ').match(/\S+/g); // breaks string into array
+    switch (t[0]) {
+        case "stop":
+        case "exit":
+            process.exit(0);
+            break;
+        case "r":
+            serialPort.write('r\r')
+            break;
+
+        case "l":
+            serialPort.write('l\r')
+            break;
+        default:
+            console.log('Unknown input:'+s)
+
+    }
+
+}
