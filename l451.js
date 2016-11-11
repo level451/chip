62869a07a6ae6605978de3d3a5f428ad56e56be7
Todd <twitzel@gmail.com>
2016-11-11 10:30:13 -0700

    asdf

--- l451.js
+++ l451.js
@@ -426,13 +426,13 @@ menusys['  Set Load Start   delay min       '] = {
     menu:12,
     sub:2,
     hasdata:true,
-    charlen:4
+    charlen:7
 }
 menusys['  Set Load Stop    delay min       '] = {
     menu:12,
     sub:3,
     hasdata:true,
-    charlen:4
+    charlen:8
 }
 menusys['  Set 24 hr start  volts DC        '] = {
     menu:12,
