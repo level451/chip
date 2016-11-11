8e669bbb1079375640db01838ea25ff0260b52d0
Todd <twitzel@gmail.com>
2016-11-10 18:28:51 -0700

    asdf

--- l451.js
+++ l451.js
@@ -85,7 +85,7 @@ function openSerialPort(portname)
                 if (sbuffer.length >= menusys[display].charlen){
                     data = sbuffer.substr(0, menusys[display].charlen)
                     data = data.replace(' ','-')
-                    console.log(data[0].charCodeAt(0))
+                    console.log(data[1].charCodeAt(0))
                     if (data.length >0){
                         console.log(display+'*Data:'+data+':'+data.length)
                         menusys[display].data = data;
