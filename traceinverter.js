2468e2f8ea2e52afa552d1ca66fe7223ca389c88
Todd <twitzel@gmail.com>
2016-11-14 09:46:41 -0700

    asdf

--- traceinverter.js
+++ traceinverter.js
@@ -56,7 +56,7 @@ function openSerialPort(portname,scb)
        if (t){
            clearTimeout(t);
        }
-//console.log(data)
+console.log(data)
 
         sbuffer += data;
         if (sbuffer.indexOf('\r')  != -1){
