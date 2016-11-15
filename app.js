985b3ae11b699c135c0fe8c7b0302636849ed469
Todd <twitzel@gmail.com>
2016-11-15 10:32:44 -0700

    asdf

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /**
  * Created by todd on 11/11/2016.
  */
-var trace = require('./traceinverter');
-trace.start(function(){
-    //trace.getInverterValue(4,2,testcallback)
-
-
-})
+// var trace = require('./traceinverter');
+// trace.start(function(){
+//     //trace.getInverterValue(4,2,testcallback)
+//
+//
+// })
 var ob = require('./outback');
 ob.start(function(){
     //trace.getInverterValue(4,2,testcallback)
