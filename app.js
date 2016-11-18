479d80577466c15cf66e88f93197a89ac3fd2e3c
Todd <twitzel@gmail.com>
2016-11-18 12:00:53 -0700

    asdf

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 /**
  * Created by todd on 11/11/2016.
  */
-// var trace = require('./traceinverter');
-// trace.start(function(){
-//     //trace.getInverterValue(4,2,testcallback)
-//
-//
-// })
+
+process.stdin.on('readable', () => {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+        commandline(chunk);
+    }
+});
+
+
 var xa = require('./xantrex');
 xa.start(function(){
     //trace.getInverterValue(4,2,testcallback)
@@ -24,5 +27,36 @@ function testcallback(d){
     console.log(JSON.stringify(d,null,4))
 
 
+
+}
+function commandline(s){
+    s = s.toString();
+    t = s.replace(/,/g,' ').match(/\S+/g); // breaks string into array
+    // console.log(t.length)
+    // for (i = 0; i < t.length; i++){
+    //     console.log(i,t[i])
+    //
+    // }
+    switch (t[0]) {
+        case "stop":
+        case "exit":
+            process.exit(0);
+            break;
+        case "send":
+            if (t[2] == null) {
+                xa.write(t[1].toUpperCase() + '\r');
+            }
+            else
+            {
+                xa.write(t[1].toUpperCase()+' '+t[2]+'\r');
+            }
+
+
+            break;
+        default:
+
+            console.log('Unknown input:'+s)
+
+    }
 
 }
