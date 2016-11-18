082459b13cce7e78a1c8dbd22988d547fac67e27
Todd <twitzel@gmail.com>
2016-11-18 12:05:08 -0700

    asdf

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ xa.start(function(){
 
     xa.write('MEASENGY?\r')
     xa.write('MEASIN?\r')
-    xa.write('MONALL?\r')
-    xa.write('FAULTLOG?\r')
+
 
 
 
@@ -32,31 +31,42 @@ function testcallback(d){
 function commandline(s){
     s = s.toString();
     t = s.replace(/,/g,' ').match(/\S+/g); // breaks string into array
+
+
+            if (t[1] == null) {
+                xa.write(t[0].toUpperCase() + '\r');
+            }
+            else
+            {
+                xa.write(t[0].toUpperCase()+' '+t[1]+'\r');
+            }
+
+
     // console.log(t.length)
     // for (i = 0; i < t.length; i++){
     //     console.log(i,t[i])
     //
     // }
-    switch (t[0]) {
-        case "stop":
-        case "exit":
-            process.exit(0);
-            break;
-        case "send":
-            if (t[2] == null) {
-                xa.write(t[1].toUpperCase() + '\r');
-            }
-            else
-            {
-                xa.write(t[1].toUpperCase()+' '+t[2]+'\r');
-            }
-
-
-            break;
-        default:
-
-            console.log('Unknown input:'+s)
-
-    }
+    // switch (t[0]) {
+    //     case "stop":
+    //     case "exit":
+    //         process.exit(0);
+    //         break;
+    //     case "send":
+    //         if (t[2] == null) {
+    //             xa.write(t[1].toUpperCase() + '\r');
+    //         }
+    //         else
+    //         {
+    //             xa.write(t[1].toUpperCase()+' '+t[2]+'\r');
+    //         }
+    //
+    //
+    //         break;
+    //     default:
+    //
+    //         console.log('Unknown input:'+s)
+    //
+    // }
 
 }
