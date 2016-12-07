283716a253a86de86a494d6804630a7724a4d3f4
Todd <twitzel@gmail.com>
2016-12-07 16:51:56 -0700

    asdf

--- app.js
+++ app.js
@@ -10,12 +10,63 @@ process.stdin.on('readable', () => {
 });
 
 
-function testcallback(d){
-    console.log(JSON.stringify(d,null,4))
+var com = require('serialport');
+//openSerialPort('/dev/ttyS0');
+
+
+    openSerialPort('/dev/ttyS0');
+
+
+console.log('working?');
 
 
 
+
+function openSerialPort(portname)
+{
+    // console.log("Attempting to open serial port "+portname);
+    // serialport declared with the var to make it module global
+    if (portname == undefined) {
+        console.log("Serial port not specified as command line - no serial port open");
+        return;
+
+    }
+    //serialPort = new com.SerialPort(portname, {
+    serialPort = new com(portname, {
+        baudrate: 115200,
+// Set the object to fire an event after a \r (chr 13 I think)  is in the serial buffer
+        parser: com.parsers.readline("\r")
+    });
+
+
+
+// I dont understand this call 0 but it works
+    serialPort.on("open", function (err,res) {
+        serialPort.set({dtr:true,rts:false});
+        console.log("Port open success:"+portname);
+
+        //serialPort.write('r\r')
+        //serialPort.write("VLD# 1 65 1 0\r");
+    });
+
+    serialPort.on('data', function(data) {
+
+        console.log(data);
+    });
+
+
+    serialPort.on('error', function(error) {
+        console.error("serial port failed to open:"+error);
+
+    });
 }
+exports.write = function(data) {
+    serialPort.write(data,function(err, results)
+    {
+
+    });
+};
+
 function commandline(s){
     s = s.toString();
     t = s.replace(/,/g,' ').match(/\S+/g); // breaks string into array
@@ -28,33 +79,4 @@ function commandline(s){
             {
                 xa.write(t[0].toUpperCase()+' '+t[1]+'\r');
             }
-
-
-    // console.log(t.length)
-    // for (i = 0; i < t.length; i++){
-    //     console.log(i,t[i])
-    //
-    // }
-    // switch (t[0]) {
-    //     case "stop":
-    //     case "exit":
-    //         process.exit(0);
-    //         break;
-    //     case "send":
-    //         if (t[2] == null) {
-    //             xa.write(t[1].toUpperCase() + '\r');
-    //         }
-    //         else
-    //         {
-    //             xa.write(t[1].toUpperCase()+' '+t[2]+'\r');
-    //         }
-    //
-    //
-    //         break;
-    //     default:
-    //
-    //         console.log('Unknown input:'+s)
-    //
-    // }
-
 }
